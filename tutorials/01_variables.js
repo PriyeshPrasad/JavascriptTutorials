2e682693bc227a371ea8c4bc79634963eecbcb09
PriyeshPrasad <45517552+PriyeshPrasad@users.noreply.github.com>
2025-08-03 00:18:18 +0000

    JS 01_variables

---
+++ tutorials/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountName = "TestName"
+var accountDetails = "Some Details"
+accountEmail = "a@b.com"
+
+//accountId = 4567 // This is not allowed. You cannot change the value of const
+
+/*
+var has issues with scope, so var is not recommended to use in JS
+*/
+
+console.table[accountId, accountName, accountDetails, accountEmail];
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountDetails);
+console.log(accountEmail);
+
+
+
